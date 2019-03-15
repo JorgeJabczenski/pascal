12e2d550543c9059746c745be54700b85fd4623c
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-03-15 00:34:16 -0300

    Lista 1 pronta

---
+++ Lista1/celsiusToFahrenheit/celsiusToFahrenheit.pas
@@ -0,0 +1,10 @@
+program celsiusToFahrenheit;
+
+var 
+    celsius: real;
+begin
+    write('Entre com a temperatura em graus Celsius: ');
+    read(celsius);
+
+    write((((9*celsius)/5) + 32):0:2,' graus Farenheit');
+end.
---
+++ Lista1/km2ml/km2ml.pas
@@ -0,0 +1,10 @@
+program km2ml;
+
+var
+    km : real;
+
+begin
+    write('Entre com a velocidade em km/h: ');
+    read(km);
+    write(km / 1.609344 :0:4,' ml/h');
+end.
