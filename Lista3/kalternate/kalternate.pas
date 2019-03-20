3ab290636cd53f6ad677553e7a675808ae796678
Jorge Lucas V Jabczenski <40120369+JorgeJabczenski@users.noreply.github.com>
2019-03-20 13:06:35 -0300

    Update kalternate.pas

--- Lista3/kalternate/kalternate.pas
+++ Lista3/kalternate/kalternate.pas
@@ -35,3 +35,51 @@ begin
     if not(nao) then
         writeln('Sim eh k-alternante');
 end.
+
+(*
+program kalternate;
+
+var
+	k, num, falso, modulo, moduloa : integer;
+begin
+
+	count := 0;
+	falso := 0;
+
+
+	read(k);
+	read(num);
+
+	moduloa := num mod 2;
+
+	while(num <> 0) do
+	begin
+		count := count + 1;
+		modulo := num mod 2;
+
+		if count = k then
+		begin
+		count := 0;
+			if moduloa = 0 then
+				moduloa := 1;
+			if moduloa = 1 then
+				moduloa := 0;
+		end; // end if
+
+		if moduloa <> modulo then
+		begin
+			writeln('NAO');
+			falso := 1;
+			break;
+		end; // end if
+
+		moduloa := modulo;
+		read(num);
+	end; // end while
+
+	if (falso = 0) then
+		writeln('SIM');
+end.
+
+
+*)
