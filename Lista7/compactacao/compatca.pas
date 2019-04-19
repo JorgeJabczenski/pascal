c75d2fbf86cbbea6ff40bdbaa27e466ef9ee2777
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-04-18 22:50:21 -0300

    VAMO DALE

--- Lista7/compactacao/compatca.pas
+++ Lista7/compactacao/compatca.pas
@@ -2,13 +2,33 @@ program compacta;
 
 const
 	MAX = 100;
-
 type
 	vetorInt = array[1..MAX] of longint;
 
 var
 	entrada : vetorInt;
 	tam,i : longint;
+
+procedure shift(var v : vetorInt; var fim,inicio : longint);
+var 
+	c : longint;
+begin
+	//writeln('SHIFT!!!');
+	//for c := 1 to fim do  write (v[c],' ');
+	//writeln;
+
+
+	for c := inicio to fim do 
+	begin
+		v[c] := v[c + 1]
+	end;
+	fim := fim - 1;
+	//inicio := inicio -1;
+
+	//for c := 1 to fim do  write (v[c],' ');
+	//writeln;
+end;
+
 
 procedure lerVetor(quantidade:longint; var v :vetorInt);
 var
@@ -30,7 +50,7 @@ end; //end procedure lerVetor
 procedure compactaVetor(quantidade : longint; var v : vetorInt);
 var 
 	aux : vetorInt;
-	i,j,ia : integer;
+	i,j,ia : longint;
 	repetido : boolean;
 
 begin
@@ -40,25 +60,27 @@ begin
 		repetido := false; j := i+1;
 		while(j <= quantidade) and (not repetido) do
 		begin
+			//writeln('Estou comparando',v[i],' com ',v[j]);
 			if v[i] = v[j] then 
 			begin
-				repetido := true;
-			end; // end if
-			j := j +1; 
+				//repetido := true;
+				shift(v,quantidade,j);
+			end 
+			else  
+				j := j +1; 
 		end; // end while
 
 		if not repetido then 
 		begin
 			ia := ia + 1;
 			aux[ia] := v[i];
-			//writeln ('AQUI O : IA = ',ia,' E AUX[IA] = ',aux[ia]);
 		end;
 	end; // end for i 
 
 	write('C: ');
-	for i := 1 to ia do
+	for i := 1 to quantidade do
 	begin
-		v[i] := aux[i];
+		//v[i] := aux[i];
 		write(v[i],' ');
 	end;
 	writeln;
