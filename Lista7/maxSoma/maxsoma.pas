d7929d3f71505197f9c6392ca3d3dcddb61253d5
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-04-19 17:10:15 -0300

    SOMADO OTARIO

--- Lista7/maxSoma/maxsoma.pas
+++ Lista7/maxSoma/maxsoma.pas
@@ -16,43 +16,29 @@ var
         end;    
     end;
 
-procedure imprimirVetor(v:Aint; t:integer);
-var i : integer;
-begin
-    for i:= 1 to t do write(v[i],' ');
-    writeln;   
-end;
-
 procedure somar(v:Aint; a,t : integer;var m:integer);
 var i,j, sominha : integer;
 begin
-    for i := 1 to t-(a-1) do // Faz a quatidade de loops baseado nos agrupamentos 
+    for i := 1 to t-(a-1) do    // Faz a quatidade de loops baseado nos agrupamentos 
     begin
         sominha := 0;
-        for j:= i to i+(a-1) do 
+        for j:= i to i+(a-1) do     // Mesma coisa que ali em cima
         begin
             sominha := sominha + v[j];
         end;   
-        //writeln('SOMINHA ',i,' := ',sominha);
-        if sominha > m then m := sominha;
+        if sominha > m then m := sominha;   // Verifica se é maior que a maior soma até agora
     end;
-
 end;
 
 begin
     read(tamanho);
     lerVetor(entrada,tamanho);
     
-    //Primeiro max = somar todos os elemtentos do vetor
+    
     maior := 0;
-    for i := 1 to tamanho do 
-        maior := maior + entrada[i];
-    //writeln('MAIOR por enquanto = ',maior);
+    for i := 1 to tamanho do  maior := maior + entrada[i];  //Primeiro maior = somar todos os elemtentos do vetor
 
-    for i := 1 to tamanho -1 do 
-    begin
-        somar(entrada,i,tamanho,maior);
-        //writeln('NOVO MAIOR ESSA ATE ESSA RODADA = ',maior);
-    end;
+    for i := 1 to (tamanho-1) do somar(entrada,i,tamanho,maior); // t - 1 pois o primeiro todos os elementos somados já foi feito ali em cima
+    
     writeln(maior);
 end.
