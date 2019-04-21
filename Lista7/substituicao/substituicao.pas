cd5b3dfc3e327653ae792a83a3e98bd447ca7624
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-04-21 18:29:11 -0300

    pronto

--- Lista7/substituicao/substituicao.pas
+++ Lista7/substituicao/substituicao.pas
@@ -53,8 +53,20 @@ begin
     begin
         posi := i;
         posf := i + t2;
-        writeln('POSI = ', posi, 'POSF = ', posf);
+        writeln('POSI = ', posi, ' || POSF = ', posf);
     end;
+end;
+
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+procedure substituirVetor(var vet1,vet3 : vetor; pi, pf : integer);
+var 
+    i : integer;
+begin
+    for i := 0 to (pf-pi) do 
+    begin
+        vet1[pi + i] := vet3[i + 1];
+    end; 
 end;
 
 //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -62,16 +74,23 @@ end;
 begin
     lerVetor(v1,t1);
     lerVetor(v2,t2);
-    //lerVetor(v3,t3);
+    lerVetor(v3,t3);
+    //imprimeVetor(v1, t1);
+    //writeln;
+    //imprimeVetor(v2, t2);
+    //writeln;
+    //imprimeVetor(v3, t3);
+    //writeln;
+
 
     if ocorre(v1, v2, t1, t2, pos_inicio,pos_final) then 
     begin
-        writeln('SIM');
-        // substitui();
-    end 
-    else
-        writeln('NAO');
+        //writeln('SIM');
+        substituirVetor(v1,v3,pos_inicio, pos_final);
+    end;
 
     //imprimeVetor(v1, t1);
+    imprimeVetor(v1, t1);
+    writeln;
 
 end.
