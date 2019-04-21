0d217657a7dbd7aa47f242a3502b6b94ab4c1df1
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-04-21 02:48:23 -0300

    enunciado

--- Lista7/subsequencias/subseq.pas
+++ Lista7/subsequencias/subseq.pas
@@ -1,3 +1,29 @@
+(*
+Faça um programa em Free Pascal que leia um número natural 0 < n ≤ 100 e em seguida leia uma 
+sequência de n números também naturais. Seu programa deve verificar se existem duas subsequências 
+guais nesta sequência com tamanho pelo menos 2. O tamanho da sequência encontrada deverá ser máximo,
+ se ela existir. Caso exista, seu programa deve imprimir o valor do ı́ndice i e do tamanho máximo da 
+ sequência m, nesta ordem, onde i é a primeira ocorrência da sequência que possui uma cópia na sequência 
+ original e m é o tamanho desta sequência que se repete. Caso contrário seu programa deve imprimir "nenhuma".
+
+Os casos de teste não conterão entradas com mais de uma subsequência igual.
+
+Exemplo de entrada 1:
+87 9 5 4 5 5 4 6
+
+Saı́da esperada para a entrada acima:
+3 2
+
+Exemplo de entrada 2:
+12
+2 7 9 5 2 5 4 8 6 2 5 4
+
+Saı́da esperada para a entrada acima:
+5 3
+
+
+*)
+
 program subseq;
 
 type vetor = array[1..200] of longint;
