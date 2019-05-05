8dfa88b468e19838058dbc65498ff3688f620079
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-05-05 20:07:06 -0300

    questao 2 prova 2018/2

--- ProvasAntigas/2018/p2/q2/arrumaVetor.pas
+++ ProvasAntigas/2018/p2/q2/arrumaVetor.pas
@@ -4,15 +4,15 @@ type
 var 
     entrada : vetor;
     tamanho : integer;
-
-procedure imprimeVetor(v : vetor; t : tamanho);
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+procedure imprimeVetor(v : vetor; t : integer);
 var i : integer;
 begin
     for i := 1 to t do write(v[i],' ');
     writeln;
 end;
-
-procedure lerVetor(var v : vetor; t : tamanho);
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+procedure lerVetor(var v : vetor; t : integer);
 var num, i : integer;
 begin
     for i := 1 to t do 
@@ -21,24 +21,74 @@ begin
         v[i] := num;
     end;
 end;
-
-procedure shiftaVetor(var v : vetor; indice : integer);
-begin 
-
-end;
-
-procedure ajeitaVetor(var v : vetor; t : integer);
-var i,j : integer;
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+procedure ordena(var v : vetor; t : integer);
+var i,j,a : integer;
 begin
-    for i := 1 to t do 
+    for i := 1 to t do  
+        for j := 1 to t-1 do 
+            if v[j] > v[j+1] then
+           begin
+                a      := v[j];
+                v[j]   := v[j+1];
+                v[j+1] := a;
+            end;
+end;
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+procedure shiftar(var v : vetor; indice, vezes : integer);
+var 
+    i : integer;
+begin
+    writeln('SHIFTANDO O VETOR...');
+    for i := 1 to vezes do 
     begin
-        for j := 1 to t do
-        begin
-            
-        end;
+        v[indice] := v[indice + 1];
+        indice := indice + 1;
     end;
 end;
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+procedure junta(var v : vetor; aux : vetor; inicio, vezes : integer);
+var i : integer;
+begin
+    writeln('JUNTANDO OS VETORES');
+    for i := 1 to vezes do 
+    begin
+        //writeln('BOTANDO ',aux[i],' NA POSICAO ',inicio);
+        v[inicio] := aux[i];
+        inicio := inicio + 1;
+    end;
+end;
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+procedure ajeitaVetor(var v : vetor; t  : integer );
+var
+    i,j,indiceAux, tam : integer;
+    aux : vetor;
+begin
+    tam := t;
+    writeln('AJEITANDO O VETOR...');
+    indiceAux := 0;
+    for i:= 1 to t do
+    begin
+        j := i + 1;
+        while j <= t  do                    // começa a checar a partir do próximo elemento a direita de v[i]
+        begin
+            if v[i] = v[j] then             // caso encontre um repetido
+            begin
+                indiceAux := indiceAux + 1; // incrementa o indice do vetor de repetidos
+                aux[indiceAux] := v[i];     // joga o numero repetido em outro vetor para ordena-lo depois
+                shiftar(v,j,(t-j));         // retira o elemento v[j] e move o resto do vetor 'pra frente'
+                t := t - 1;
+                j := j - 1;
+            end;
+            j := j + 1;
+        end;
+    end;
+    writeln;
 
+    ordena(aux,indiceAux);                      // ordena o veto de repetidos
+    junta(v,aux,(tam-indiceAux)+1, indiceAux);  // junta os vetores do jeito certo 
+end;
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 begin
     read(tamanho);
     lerVetor(entrada, tamanho);
--- Rascunhos/lista7-subsequencias/lista7-subsequencias.pas
+++ Rascunhos/lista7-subsequencias/lista7-subsequencias.pas
@@ -38,24 +38,27 @@ begin
         while(j + s <= t) and not igual do 
         begin
             igual := true;
-
             for c := 1 to s do
-                if v[i+c] <> v[j+c] then igual := false;       
-
+            begin
+                //writeln(v[i+c],'-------',v[j+c]);
+                if v[i+c] <> v[j+c] then
+                     igual := false;       
+            end;
             j := j + 1;
-        
+           // writeln;
         end;
-
         if igual then 
         begin
             continua := false;
             acharSequencia := i+1;
         end
         else
+        begin
             acharSequencia := 0;
+        end;
         i := i + 1;
-
     end;
+
 end;
 
 //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
